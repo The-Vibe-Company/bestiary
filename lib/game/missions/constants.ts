b570de5e2ff844e42eb0ff920f5049ad443949b1
Zewed <dewez.antoine2@gmail.com>
2026-02-07 19:20:25 +0100

    done

--- lib/game/missions/constants.ts
+++ lib/game/missions/constants.ts
@@ -1,3 +1,3 @@
-export const MIN_WORK_SECONDS = 1800   // 30 minutes
+export const MIN_WORK_SECONDS = 60     // 1 minute
 export const MAX_WORK_SECONDS = 28800  // 8 hours
-export const WORK_STEP_SECONDS = 1800  // 30-minute increments
+export const WORK_STEP_SECONDS = 60    // 1-minute granularity
