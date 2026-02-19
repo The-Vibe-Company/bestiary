1fa05becddd1f7b027e949c6cb4cf43166f60cc1
Zewed <dewez.antoine2@gmail.com>
2026-02-19 22:18:34 +0100

    refactor(travelers): remove no-op pending toggles on assign path

--- components/place/travelers-panel.tsx
+++ components/place/travelers-panel.tsx
@@ -25,11 +25,8 @@ export function TravelersPanel({ travelerStatus, inhabitantTypes, isVillageFull 
 
   async function handleWelcome() {
     if (isWelcomed) {
-      setPendingWelcome(true)
       setWelcomeError(null)
-      router.refresh()
       setShowModal(true)
-      setPendingWelcome(false)
       return
     }
 
